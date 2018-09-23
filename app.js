d26c0638cd1cf6b03a7fbad49c9623f43bca325a
manuelWeb <devries.webdesigner@gmail.com>
2018-09-23 10:13:07 +0200

    passage de data à handler en plus de l'obj

--- app.js
+++ app.js
@@ -9,21 +9,22 @@ class Ping{
     // filter : si faux elt supp du tableau si vrai elt conservé
     this.observers = this.observers.filter(oberser => oberser != fn)
   }
-  fire(thisObj){
+  fire(data, thisObj){
     // iter sur chaques observer du tableau
     this.observers.forEach(observer => {
       // appel oberser en passant en param l'obj this
-      observer.call(thisObj)
+      observer.call(thisObj, data)
     });
   }
 }
 // instance de l'obd
 var ping = new Ping();
 
-var handler = function () {
-  console.log(this);
+var handler = function (data) {
+  console.log(data);
+  console.log(this.message);
 }
 
 ping.subscribe(handler);
 
-ping.fire('hello');
+ping.fire('bye!!!', {message: 'Holã'});
