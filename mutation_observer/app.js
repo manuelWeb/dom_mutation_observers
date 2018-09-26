77d27031839a2d07b8a16e045172f30f555300d7
macfly <manuel@webdesignord.fr>
2018-09-26 16:46:08 +0200

    modif app.js

--- mutation_observer/app.js
+++ mutation_observer/app.js
@@ -1,27 +1,12 @@
-// Selectionne le noeud dont les mutations seront observées
-var targetNode = document.getElementById('ctl00_ContentPlaceHolder1_A_VALIDER_CODE_KDO');
+var eltToObs = document.getElementById('ctl01_ContentPlaceHolder1_A_VALIDER_CODE_KDO');
 
-// Options de l'observateur (quelles sont les mutations à observer)
-var config = { attributes: true, childList: true };
+var observer = new MutationObserver(function(mutations) {
+  mutations.forEach(function(mutation) {
+    mutation.addedNodes.forEach(function(addedNode) {
+      console.log(addedNode)
+    });
+  });
+});
 
-// Fonction callback à éxécuter quand une mutation est observée
-var callback = function (mutationsList) {
-  for (var mutation of mutationsList) {
-    if (mutation.type == 'childList') {
-      console.log('Un noeud enfant a été ajouté ou supprimé.');
-    }
-    else if (mutation.type == 'attributes') {
-      console.log("L'attribut '" + mutation.attributeName + "' a été modifié.");
-      document.getElementById('ctl00_ContentPlaceHolder1_A_VALIDER_CODE_KDO').replaceWith(targetNode)
-    }
-  }
-};
-
-// Créé une instance de l'observateur lié à la fonction de callback
-var observer = new MutationObserver(callback);
-
-// Commence à observer le noeud cible pour les mutations précédemment configurées
-observer.observe(targetNode, config);
-
-// L'observation peut être arrêtée par la suite
-observer.disconnect();
+// observer.observe(document.body, {childList: true, subtree: true});
+observer.observe(document.body, {attributes: true, childList: true, subtree: true});
